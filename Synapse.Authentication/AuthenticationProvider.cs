72830f81ebd031a3faa14e796d6b1aab3a27d690
steveshortt <steve.shortt@gmail.com>
2017-05-10 12:25:30 -0500

    code cleanup

--- Synapse.Authentication/AuthenticationProvider.cs
+++ Synapse.Authentication/AuthenticationProvider.cs
@@ -30,33 +30,33 @@ namespace Synapse.Authentication
 
         public bool IsAuthenticated(string ldapRoot, string domain, string username, string password)
         {
-            string domainAndUsername = username.StartsWith( domain + @"\" ) ? username : domain + @"\" + username;
+            //note: we found that ldap seems to require upper-case protocol declaration.  don't know why.
+            //      this will correct whatever comes in to allowable casing.
+            ldapRoot = ldapRoot.ToLower().Replace( "ldap://", "LDAP://" );
+
+            string domainAndUsername = username.ToLower().StartsWith( domain.ToLower() + @"\" ) ?
+                username : domain + @"\" + username;
             DirectoryEntry entry = new DirectoryEntry( ldapRoot, domainAndUsername, password );
 
+            bool isAuthenticated = false;
             try
             {
                 // Bind to the native AdsObject to force authentication.
                 Object obj = entry.NativeObject;
-                DirectorySearcher search = new DirectorySearcher( entry )
-                {
-                    Filter = "(sAMAccountName=" + username + ")"
-                };
+
+                DirectorySearcher search = new DirectorySearcher( entry );
+                search.Filter = "(sAMAccountName=" + username + ")";
                 search.PropertiesToLoad.Add( "cn" );
                 SearchResult result = search.FindOne();
-                if( null == result )
-                {
-                    return false;
-                }
-                // Update the new path to the user in the directory
-                ldapRoot = result.Path;
-                string filterAttribute = (String)result.Properties["cn"][0];
+
+                isAuthenticated = result != null;
             }
             catch( Exception ex )
             {
                 throw new Exception( "Error authenticating user. " + ex.Message );
             }
 
-            return true;
+            return isAuthenticated;
         }
     }
 }
