b1806dacb917d81d45bd89d93bf8ab9110c05958
steveshortt <steve.shortt@gmail.com>
2017-06-15 18:37:20 -0500

    Added StartPlanSync to IExecuteController (#34)

--- Synapse.Server.Extensibility/IExecuteController.cs
+++ Synapse.Server.Extensibility/IExecuteController.cs
@@ -10,21 +10,29 @@ namespace Synapse.Services
 {
     public interface IExecuteController
     {
-        UrlHelper CurrentUrl { get; set; }
-        IPrincipal CurrentUser { get; set; }
+        string Hello();
+        string WhoAmI();
+        IEnumerable<string> GetPlanList(string filter = null, bool isRegexFilter = true);
+        IEnumerable<long> GetPlanInstanceIdList(string planUniqueName);
 
+        long StartPlan(string planUniqueName, bool dryRun = false, string requestNumber = null, string nodeRootUrl = null);
+        long StartPlan([FromBody] StartPlanEnvelope planEnvelope, string planUniqueName, bool dryRun = false, string requestNumber = null, string nodeRootUrl = null);
+        object StartPlanSync(string planUniqueName, bool dryRun = false, string requestNumber = null,
+            string path = "Actions[0]:Result:ExitData", SerializationType serializationType = SerializationType.Json,
+            bool setContentType = true, int pollingIntervalSeconds = 1, int timeoutSeconds = 120, string nodeRootUrl = null);
+        object StartPlanSync([FromBody]StartPlanEnvelope planEnvelope, string planUniqueName, bool dryRun = false, string requestNumber = null,
+            string path = "Actions[0]:Result:ExitData", SerializationType serializationType = SerializationType.Json,
+            bool setContentType = true, int pollingIntervalSeconds = 1, int timeoutSeconds = 120, string nodeRootUrl = null);
+
+        Plan GetPlanStatus(string planUniqueName, long planInstanceId);
+        void SetStatus(string planUniqueName, long planInstanceId, [FromBody] string planString);
+        void SetStatus(string planUniqueName, long planInstanceId, [FromBody] ActionItem actionItem);
         void CancelPlan(string planUniqueName, long planInstanceId, string nodeRootUrl = null);
         object GetPlanElements(string planUniqueName, long planInstanceId, string elementPath,
             SerializationType serializationType = SerializationType.Json, bool setContentType = true);
         object GetPlanElements(string planUniqueName, long planInstanceId, [FromBody] PlanElementParms elementParms);
-        IEnumerable<long> GetPlanInstanceIdList(string planUniqueName);
-        IEnumerable<string> GetPlanList(string filter = null, bool isRegexFilter = true);
-        Plan GetPlanStatus(string planUniqueName, long planInstanceId);
-        string Hello();
-        void SetStatus(string planUniqueName, long planInstanceId, [FromBody] string planString);
-        void SetStatus(string planUniqueName, long planInstanceId, [FromBody] ActionItem actionItem);
-        long StartPlan(string planUniqueName, bool dryRun = false, string requestNumber = null, string nodeRootUrl = null);
-        long StartPlan([FromBody] StartPlanEnvelope planEnvelope, string planUniqueName, bool dryRun = false, string requestNumber = null, string nodeRootUrl = null);
-        string WhoAmI();
+
+        UrlHelper CurrentUrl { get; set; }
+        IPrincipal CurrentUser { get; set; }
     }
 }
