af389ece9182bef0c3a45b7c36ba7563935dbed1
Steve <steve.shortt@gmail.com>
2018-02-10 12:19:26 -0600

    code cleanup on UserIdProvider, added FileSource support

--- Synapse.Authorization/Classes/UserIdProvider.cs
+++ Synapse.Authorization/Classes/UserIdProvider.cs
@@ -1,17 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 using Synapse.Core.Utilities;
 using Synapse.Services;
 
+using YamlDotNet.Serialization;
 
 public class UserIdProvider : IAuthorizationProvider
 {
     public List<string> Allowed { get; set; }
+    [YamlIgnore]
+    public bool HasAllowed { get { return Allowed != null && Allowed.Count > 0; } }
+
     public List<string> Denied { get; set; }
+    [YamlIgnore]
+    public bool HasDenied { get { return Denied != null && Denied.Count > 0; } }
 
     public string ListSourcePath { get; set; }
+
 
     public Dictionary<string, string> Configure(IAuthorizationProviderConfig conifg)
     {
@@ -20,9 +28,29 @@ public class UserIdProvider : IAuthorizationProvider
             string s = YamlHelpers.Serialize( conifg.Config );
             UserIdProvider uidp = YamlHelpers.Deserialize<UserIdProvider>( s );
 
+            //initialize with whatever is declared in synapse.server.config.yaml
             Allowed = uidp.Allowed;
             Denied = uidp.Denied;
             ListSourcePath = uidp.ListSourcePath;
+
+            //if external source declared, merge contents
+            if( !string.IsNullOrWhiteSpace( ListSourcePath ) && File.Exists( ListSourcePath ) )
+            {
+                UserIdProvider listSource = YamlHelpers.DeserializeFile<UserIdProvider>( ListSourcePath );
+                if( listSource.HasAllowed )
+                {
+                    if( Allowed == null )
+                        Allowed = new List<string>();
+                    Allowed.AddRange( listSource.Allowed );
+                }
+
+                if( listSource.HasDenied )
+                {
+                    if( Denied == null )
+                        Denied = new List<string>();
+                    Denied.AddRange( listSource.Denied );
+                }
+            }
         }
 
         return null;
@@ -37,12 +65,12 @@ public class UserIdProvider : IAuthorizationProvider
     {
         bool? found = null;
 
-        if( Denied != null && Denied.Count > 0 )
+        if( HasDenied )
             found = Denied.Contains( id, StringComparer.OrdinalIgnoreCase );
         if( found.HasValue && found.Value )
             return false;
 
-        if( Allowed != null && Allowed.Count > 0 )
+        if( HasAllowed )
             found = Allowed.Contains( id, StringComparer.OrdinalIgnoreCase );
         if( found.HasValue && found.Value )
             return true;
